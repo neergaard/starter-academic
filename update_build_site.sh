56e3f17aa02ea17139054e5f42637743b35d7a2f
Alexander Neergaard Olesen <alexander.neergaard@gmail.com>
2021-02-17 13:13:48 +0100

    update site

--- update_build_site.sh
+++ update_build_site.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 # Update the site
+echo ""
+echo "Updating site with changes..."
 git add .
 git commit -m "update site"
 git push -u origin master
 
 # Build and push site
+echo ""
+echo "Building site"
 hugo
 cd public
 git add .
 git commit -m "build site"
 git push origin master
 cd ..
+echo ""
+echo "Site fully updated and built."
